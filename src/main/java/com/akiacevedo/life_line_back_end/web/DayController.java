c2c69bf7ea7ce389bc030e10ccf02ba7dedbe397
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-18 13:55:52 +0200

    adding service to the controller

--- src/main/java/com/akiacevedo/life_line_back_end/web/DayController.java
+++ src/main/java/com/akiacevedo/life_line_back_end/web/DayController.java
@@ -1,9 +1,24 @@
 package com.akiacevedo.life_line_back_end.web;
 
+import com.akiacevedo.life_line_back_end.model.Day;
+import com.akiacevedo.life_line_back_end.service.DayService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/days")
 public class DayController {
+
+    private final DayService service;
+
+    @Autowired
+    public DayController(DayService service) {
+        this.service = service;
+    }
+
 }
