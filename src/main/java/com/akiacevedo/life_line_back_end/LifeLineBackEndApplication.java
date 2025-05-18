03b493b05dc9dcf5e3100cca98ffb96f7cedf5bd
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-18 13:50:34 +0200

    removed unused code from application

--- src/main/java/com/akiacevedo/life_line_back_end/LifeLineBackEndApplication.java
+++ src/main/java/com/akiacevedo/life_line_back_end/LifeLineBackEndApplication.java
@@ -1,21 +1,13 @@
 package com.akiacevedo.life_line_back_end;
 
-import com.akiacevedo.life_line_back_end.model.Day;
-import com.akiacevedo.life_line_back_end.service.DayService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LifeLineBackEndApplication {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context =  SpringApplication.run(LifeLineBackEndApplication.class, args);
-		DayService service = context.getBean(DayService.class);
-		System.out.println(service.getDays());
-		Day test  = new Day();
-		test.setDate("");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LifeLineBackEndApplication.class, args);
+    }
 
 }
