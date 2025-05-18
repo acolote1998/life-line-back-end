63b50dc049f82255a774991d292fb43faf900c2f
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-18 12:35:29 +0200

    initialising project

---
+++ src/main/java/com/akiacevedo/life_line_back_end/LifeLineBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.akiacevedo.life_line_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeLineBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeLineBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akiacevedo/life_line_back_end/LifeLineBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akiacevedo.life_line_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeLineBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
