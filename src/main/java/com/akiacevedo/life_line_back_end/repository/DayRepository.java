c35288019a3d99e5f3562efd1f9b6e67e1c06bed
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-18 12:48:39 +0200

    autowiring repositori into service

--- src/main/java/com/akiacevedo/life_line_back_end/repository/DayRepository.java
+++ src/main/java/com/akiacevedo/life_line_back_end/repository/DayRepository.java
@@ -1,11 +1,12 @@
 package com.akiacevedo.life_line_back_end.repository;
 
 import com.akiacevedo.life_line_back_end.model.Day;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 import static com.akiacevedo.life_line_back_end.repository.DayReader.ReadDays;
-
+@Repository
 public class DayRepository {
     private List<Day> days = ReadDays();
 
--- src/main/java/com/akiacevedo/life_line_back_end/service/DayService.java
+++ src/main/java/com/akiacevedo/life_line_back_end/service/DayService.java
@@ -1,5 +1,15 @@
 package com.akiacevedo.life_line_back_end.service;
 
-public class DayService {
+import com.akiacevedo.life_line_back_end.repository.DayRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+public class DayService {
+private final DayRepository repository;
+
+@Autowired
+    public DayService(DayRepository repository) {
+        this.repository = repository;
+    }
 }
