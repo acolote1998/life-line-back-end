2547aba9c5f1d257b589709d71cfad2909a0fbd6
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-18 13:53:31 +0200

    initialising day controller

--- src/main/java/com/akiacevedo/life_line_back_end/web/DayController.java
+++ src/main/java/com/akiacevedo/life_line_back_end/web/DayController.java
@@ -1,4 +1,9 @@
 package com.akiacevedo.life_line_back_end.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/days")
 public class DayController {
 }
