30368f29d5dda81558e8abfdda65a64f5d7f5ce4
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-23 10:56:03 +0200

    created day repository. find all and find by id

--- src/main/java/com/akiacevedo/life_line_back_end/repository/DayRepository.java
+++ src/main/java/com/akiacevedo/life_line_back_end/repository/DayRepository.java
@@ -1,23 +1,12 @@
 package com.akiacevedo.life_line_back_end.repository;
 
 import com.akiacevedo.life_line_back_end.model.Day;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.List;
 
-import static com.akiacevedo.life_line_back_end.repository.DayFileManager.ReadDays;
-import static com.akiacevedo.life_line_back_end.repository.DayFileManager.WriteDays;
+public interface DayRepository extends ListCrudRepository<Day, Long> {
+    List<Day> findAll();
 
-@Repository
-public class DayRepository {
-    private List<Day> days = ReadDays();
-
-    public List<Day> getDays() {
-        return days;
-    }
-
-    public void setDays(List<Day> days) {
-        WriteDays(days);
-        this.days = ReadDays();
-    }
+    Day findDayById(Long id);
 }
