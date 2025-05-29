b80a6c36c6449624ec5608a7d1c09ec9d23100c1
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-29 10:06:29 +0200

    adding spring security filter, allowing all requests for now

---
+++ src/main/java/com/akiacevedo/life_line_back_end/web/DaySecurity.java
@@ -0,0 +1,30 @@
+package com.akiacevedo.life_line_back_end.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class DaySecurity {
+
+    @Bean
+    public SecurityFilterChain securityFilter(HttpSecurity http) throws Exception {
+        http
+                .csrf((crsf -> crsf.disable()))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/days").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/days").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/days/*").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .cors(withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
+        return http.build();
+    }
+}
